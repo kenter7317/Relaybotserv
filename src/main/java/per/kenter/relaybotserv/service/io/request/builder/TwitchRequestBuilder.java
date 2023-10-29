b6b3e7ceb2e1ca9708f73f544caca94b490292ed
kenter7317 <kenter7317@gmail.com>
2023-10-29 23:28:22 +0900

    add add TwitchRequestBuilder Constructor

--- src/main/java/per/kenter/relaybotserv/service/io/request/builder/TwitchRequestBuilder.java
+++ src/main/java/per/kenter/relaybotserv/service/io/request/builder/TwitchRequestBuilder.java
@@ -1,4 +1,10 @@
 package per.kenter.relaybotserv.service.io.request.builder;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class TwitchRequestBuilder {
 }
