1e4dc2bea9c8a414d84f4335c35f62072c45c008
kenter7317 <kenter7317@gmail.com>
2023-11-09 04:51:06 +0900

    refator refactor package and svc

--- src/main/java/per/kenter/relaybotserv/controller/rest/PostController.java
+++ src/main/java/per/kenter/relaybotserv/controller/rest/PostController.java
@@ -2,35 +2,42 @@ package per.kenter.relaybotserv.controller.rest;
 
 
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import per.kenter.relaybotserv.service.io.request.dto.RequestWebhookData;
 import per.kenter.relaybotserv.service.io.request.dto.VerificationData;
+import per.kenter.relaybotserv.service.io.response.RestResponseService;
 
 import java.io.IOException;
+
 
 
 import static per.kenter.relaybotserv.service.io.request.RestRequestService.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/post")
 public class PostController {
 
 
     @PostMapping("/callback")
     public void callback(HttpServletResponse response, @RequestBody VerificationData verificationData) throws IOException {
-        setValidResponse(response);
-        classifyData(response, verificationData);
+        RestResponseService.setValidResponse(response);
+        RestResponseService.classifyData(response, verificationData);
+    }
+
+    @PostMapping("/ad/getUsers")
+    public void getUsers(HttpServletResponse response) throws IOException {
+        RestResponseService.setValidResponse(response);
+    }
+
+    @PostMapping("/ad/getOAuthAC")
+    public void getOAuthAC(HttpServletResponse response) throws IOException {
+        RestResponseService.setValidResponse(response);
     }
 
 
-
-
-
     @PostMapping("/reqsub")
-    public void requestEventSub(@RequestBody RequestWebhookData REQEventSub) throws IOException, InterruptedException {
+    public void requestEventSub( HttpServletResponse response, @RequestBody RequestWebhookData REQEventSub) throws IOException, InterruptedException {
+        RestResponseService.setValidResponse(response);
         requestEventSubForStream(REQEventSub);
     }
 }
