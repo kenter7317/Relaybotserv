9080577b849f9914a43283bf1764e44468728fc2
kenter7317 <kenter7317@gmail.com>
2023-11-15 17:29:10 +0900

    add add AccountSNSService and DataProcessUtil

---
+++ src/main/java/per/kenter/relaybotserv/service/AccountSNSService.java
@@ -0,0 +1,19 @@
+package per.kenter.relaybotserv.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import per.kenter.relaybotserv.repository.OauthAccountRepository;
+@RequiredArgsConstructor
+@Service
+public class AccountSNSService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final OauthAccountRepository repository;
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        return null;
+    }
+}
